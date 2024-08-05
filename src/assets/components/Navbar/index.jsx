387f74a5a1ce8b20db0e72ae78810f005ef004a3
Carl Åberg <caaberg99@gmail.com>
2024-08-06 01:54:05 +0200

    more img adjustments

--- src/assets/components/Navbar/index.jsx
+++ src/assets/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ setCurrentPage }) => (
         <nav class=" bg-white flex sticky top-3 z-99 h-[60px] right-0 mx-auto items-center justify-between rounded-2xl border-b-2 px-8 shadow-sm saturate-100 lg:w-[70%]">
         <a class="2rem" href="/">
             <svg width="2rem" height="2rem" xmlns="http://www.w3.org/2000/svg">
-            <image href="./src/assets/images/Carlicon.png" height="2rem" width="2rem"/>
+            <image href="/images/CarlIcon.png" height="2rem" width="2rem"/>
             </svg>
         </a>
         <div class="lg:flex items-center gap-2">
